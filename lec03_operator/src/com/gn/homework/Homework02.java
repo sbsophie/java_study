4a05339e838db8d9b703fa0d70c718cb03949dd7
sbsophie <devsophie37@gmail.com>
2024-11-28 17:47:31 +0900

    [psb] Homework(연산자)

---
+++ lec03_operator/src/com/gn/homework/Homework02.java
@@ -0,0 +1,16 @@
+package com.gn.homework;
+
+public class Homework02 {
+
+	public static void main(String[] args) {
+//		구슬의 개수를 키보드로 입력 받으세요.
+//		그리고, 구슬의 개수가 짝수인지 홀수인지 출력하세요.
+//		특정 숫자를 2로 나눴을때 나머지가 0이면 짝수, 1이면 홀수입니다.
+//		
+//		구슬의 개수 : 3
+//		구슬의 개수는 홀수입니다.
+		
+		
+	}
+
+}
