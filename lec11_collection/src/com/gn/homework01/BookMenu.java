0385ceeb6acab9fa72878fc433c994f34205a3b4
sbsophie <devsophie37@gmail.com>
2024-12-11 17:52:23 +0900

    [psb] 과제

--- lec11_collection/src/com/gn/homework01/BookMenu.java
+++ lec11_collection/src/com/gn/homework01/BookMenu.java
@@ -7,6 +7,13 @@ public class BookMenu {
 	private BookController bc = new BookController();
 	
 	public void mainMenu() {
+		System.out.println("=== 가남 도서관에 오신걸 환영합니다 ===\n원하시는 업무의 번호를 선택하세요");
+		System.out.println("1. 새 도서 추가");
+		System.out.println("2. 도서 전체 조회");
+		System.out.println("3. 도서 검색 조회");
+		System.out.println("4. 도서 삭제");
+		System.out.println("5. 도서 오름차순 정렬");
+		System.out.println("9. 종료");
 		System.out.println("메뉴 선택: ");
 		int select = sc.nextInt();
 		//contain사용해보기
