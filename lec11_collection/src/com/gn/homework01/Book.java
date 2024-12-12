922a0ca916b29690d4b9bd389e231c7bcecbb5ea
sbsophie <devsophie37@gmail.com>
2024-12-12 20:22:17 +0900

    과제연습

--- lec11_collection/src/com/gn/homework01/Book.java
+++ lec11_collection/src/com/gn/homework01/Book.java
@@ -15,14 +15,30 @@ public class Book {
 		this.price = price;
 	}
 	
-	public String getTitile() {
-		
+	public String getTitle() {
+		return title;
 	}
-	
-	
-	
-	
-	
+	public String getAuthor() {
+		return author;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
 	
 	
 	public String toString() {
