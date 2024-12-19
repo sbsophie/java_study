dfd5426bd5cd5f8b5dd8f21fbd749e20f245b989
sbsophie <devsophie37@gmail.com>
2024-12-19 17:46:41 +0900

    [psb] IO stream 과제

--- lec13_io/src/com/gn/homework01/Practice.java
+++ lec13_io/src/com/gn/homework01/Practice.java
@@ -1,5 +1,6 @@
 package com.gn.homework01;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -15,10 +16,11 @@ public class Practice {
 		FileOutputStream fos = null;
 		try {
 			fos = new FileOutputStream(file);
-			System.out.println(song);
-			
-			
-			
+			String str = song;
+//			byte[] arr = song.getBytes();
+//			fos.write(arr);
+//			fos.flush();
+			System.out.println(str);
 			
 		}catch(IOException e) {
 			e.printStackTrace();
@@ -29,6 +31,38 @@ public class Practice {
 				e.printStackTrace();
 			}
 		}
+	
+		return song;
+	}
+	
+	public String method2(String song) {
+		File dir = new File("C:\\test\\sub");
+		if(dir.exists()) {
+			dir.mkdirs();
+		}
+		File file = new File(dir,"나비야2.dat");
+		
+		FileOutputStream fos = null;
+		BufferedOutputStream bf = null;
+		try {
+			fos = new FileOutputStream(file);
+			bf = new BufferedOutputStream(fos);
+			String str = song;
+			System.out.println(song);
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(bf != null) bf.close();
+				if(fos != null) fos.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
 		
 		
 		
@@ -36,10 +70,6 @@ public class Practice {
 		
 		return song;
 	}
-	
-//	public String method2(String song) {
-//		return song;
-//	}
 	
 	
 	
--- lec13_io/src/com/gn/homework01/Run.java
+++ lec13_io/src/com/gn/homework01/Run.java
@@ -1,17 +1,24 @@
 package com.gn.homework01;
 
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+
 public class Run {
 	public static void main(String[] args) {
+		LocalTime startTime = LocalTime.now();
+		LocalTime endTime = LocalTime.of(0, 0);
+//		Calendar time = Calendar.getInstance();
+//		long diff = time.getTimeInMillis();
+		
 		Practice p = new Practice();
 		String song = "나비야, 나비야 이리 날아오너라"
 				+ "\n노랑나비, 흰 나비 춤을 추며 오너라"
 				+"\n봄바람에 꽃잎도 방긋방긋 웃으며"
 				+ "\n참새도 짹짹짹 노래하며 춤춘다";
 		p.method1(song);
-//		p.method2(song);
+		p.method2(song);
 		
-		
-		
-		
+		long timeBetween = ChronoUnit.NANOS.between(startTime,endTime);
 	}
 }
