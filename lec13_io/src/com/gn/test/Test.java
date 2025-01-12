6dd823c1f076be920b1d4e9d82463882f2ea3ae5
sbsophie <devsophie37@gmail.com>
2025-01-13 00:15:47 +0900

    for문 연습

--- lec13_io/src/com/gn/test/Test.java
+++ lec13_io/src/com/gn/test/Test.java
@@ -3,29 +3,13 @@ package com.gn.test;
 public class Test {
 
 	public static void main(String[] args) {
-		
-//		for(int i = 1; i<= 100; i++) {
-//				if(i % 3 == 0 || i % 5 == 0) {
-//					System.out.println(i);
-//				}
-//		}
 
-//		for(int i =1; i <=50; i++) {
-//			if(i % 7 ==0) {
-//				System.out.println(i);
-//			}
-//		}
-		int sum =0;
-		for(int i=1; i <=20; i++) {
-			if(i % 2 == 1) {
-				sum += i;
+		for(int i = 2; i <=9; i++) {
+			System.out.println("=== "+i+"단 ===");
+			for(int j = 1; j <=9; j++) {
+				System.out.println(i+" * "+j+ " = " +j*i);
 			}
 		}
-		System.out.println(sum);
-		
-		
-		
-		
 		
 	}
 }
