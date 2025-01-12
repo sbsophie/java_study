590bddf589ce134b360de29705a37bda293b6b45
sbsophie <devsophie37@gmail.com>
2025-01-13 01:05:12 +0900

    for문 연습

--- lec13_io/src/com/gn/test/Test.java
+++ lec13_io/src/com/gn/test/Test.java
@@ -4,11 +4,11 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		for(int i = 2; i <=9; i++) {
-			System.out.println("=== "+i+"단 ===");
-			for(int j = 1; j <=9; j++) {
-				System.out.println(i+" * "+j+ " = " +j*i);
+		for(int i = 1; i <=5; i++) {
+			for(int j =1; j <=i; j++) {
+				System.out.print("*");
 			}
+			System.out.println();
 		}
 		
 	}
