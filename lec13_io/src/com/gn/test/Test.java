29e831cc8ef7a05133187f85bba45c55814d03a4
sbsophie <devsophie37@gmail.com>
2025-01-29 00:57:57 +0900

    [psb] 코테연습

--- lec13_io/src/com/gn/test/Test.java
+++ lec13_io/src/com/gn/test/Test.java
@@ -4,19 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		// int solution(int n)
-		// n 10 / result 30
-		// n 4 / result 6
-//		int arr[] = {n.length};
-		int sum = 0;
-		for(int i= 0; i <=10; i++){
-			if(i % 2 == 0) {
-				sum += i;
-			}
-		}
-		System.out.println("total"+sum);
-		
-		
+
 		
 	}
 }
