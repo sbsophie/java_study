265b672ee29b1611016640d4711f8c00d55e42f7
sbsophie <devsophie37@gmail.com>
2025-01-28 13:35:50 +0900

    [psb] macbook으로 연

--- lec13_io/src/com/gn/test/Test.java
+++ lec13_io/src/com/gn/test/Test.java
@@ -3,13 +3,20 @@ package com.gn.test;
 public class Test {
 
 	public static void main(String[] args) {
-
-		for(int i = 1; i <=5; i++) {
-			for(int j =1; j <=i; j++) {
-				System.out.print("*");
+		
+		// int solution(int n)
+		// n 10 / result 30
+		// n 4 / result 6
+//		int arr[] = {n.length};
+		int sum = 0;
+		for(int i= 0; i <=10; i++){
+			if(i % 2 == 0) {
+				sum += i;
 			}
-			System.out.println();
 		}
+		System.out.println("total"+sum);
+		
+		
 		
 	}
 }
